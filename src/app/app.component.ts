3129be5b5001e129721b9f37717fa99393240287
nicic <nicicdamjan@gmail.com>
2018-12-02 23:30:21 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './service/user-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'isaFrontend';
+
+  objekat = {};
+  constructor(private userService: UserService) {
+    
+  }
+
+  ngOnInit() {
+    console.log(`evo me`);
+    this.userService.get().toPromise().then(data => {
+      console.log('kao tu sam');
+      this.objekat = data});
+  }
+
+  stameni = "SUPCINA";
 }
