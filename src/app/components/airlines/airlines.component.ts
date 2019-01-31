a2e07e79b7bc697c8690a5be543d0bc874d14db6
LosBlancos6 <stefans996@gmail.com>
2019-01-31 18:28:52 +0100

    html for airline list

--- src/app/components/airlines/airlines.component.ts
+++ src/app/components/airlines/airlines.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AirlineService } from './../../shared-service/airline.service';
+import { Observable } from 'rxjs';
+import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+
 
 @Component({
   selector: 'app-airlines',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./airlines.component.css']
 })
 export class AirlinesComponent implements OnInit {
+  
+  airlines = [];
 
-  constructor() { }
+  constructor( private airlineService: AirlineService, protected router: Router) { }
 
   ngOnInit() {
+    this.airlineService.getAirlines().subscribe(
+      data => {
+        this.airlines = data;
+      }
+
+    );
   }
+
+ 
+
+ 
 
 }
