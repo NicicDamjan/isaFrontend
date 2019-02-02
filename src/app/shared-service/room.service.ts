5cbfa4b98d4fcc75abe478f04a78c6b64c5b242d
nicic <nicicdamjan@gmail.com>
2019-02-02 05:16:19 +0100

    dopuna room.service

--- src/app/shared-service/room.service.ts
+++ src/app/shared-service/room.service.ts
@@ -18,5 +18,20 @@ export class RoomService {
   getRoom(id: number): Observable<any> {
      return this.http.get(`${this.BASE_URL}/rooms/` + id);
   }
+  deleteRoom(roomId: number): Observable<any> {
+    return this.http.delete(`${this.BASE_URL}/rooms/` + roomId);
+  }
+
+  editRoom(room: Room, roomId: number): Observable<any> {
+    const body = JSON.stringify(room);
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.put(`${this.BASE_URL}/rooms/` + roomId, body, {headers: headers});
+  }
+
+  addNewRoom(room: Room, id: number): Observable<any> {
+    const body = JSON.stringify(room);
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post(`${this.BASE_URL}/hotels/` + id + '/rooms', body, {headers: headers} );
+  }
 
 }
