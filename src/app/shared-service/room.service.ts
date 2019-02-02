13de4c2da813f9706f64eb4ac8c003d5ac438a31
nicic <nicicdamjan@gmail.com>
2019-02-02 05:14:28 +0100

    funkcija u room service za getovanje svih i pojeinacne sobe

--- src/app/shared-service/room.service.ts
+++ src/app/shared-service/room.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Room} from '../Room';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'}),
+};
 
 @Injectable()
 export class RoomService {
+  private  BASE_URL = 'http://localhost:8090/api';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAllRooms(id: number): Observable<any> {
+    return this.http.get(`${this.BASE_URL}/hotels/` + id + '/rooms', httpOptions);
+  }
+  getRoom(id: number): Observable<any> {
+     return this.http.get(`${this.BASE_URL}/rooms/` + id);
+  }
 
 }
